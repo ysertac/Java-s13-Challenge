550d18e50e48074a4b5ecf58a2d45e0eae12768b
Dogancan94 <dogancankinik@gmail.com>
2023-06-02 09:03:33 +0300

    Nesne Tabanlı Programlama Temelleri Örnek

---
+++ src/employeeApp/Main.java
@@ -0,0 +1,31 @@
+package employeeApp;
+
+/**
+ * Uygulamanın amacı Java'da nesne tabanlı programlamanın temellerinin oluşturulmasıdır.
+ *
+ */
+public class Main
+{
+
+    /**
+     * Uygulamadaki tüm işlemler bu metotda yapılmalı
+     */
+    private static void workWithData ()
+    {
+
+    }
+
+    /**
+     * Bu projenin ana(main) metodu. Java uygulamalarında main metod küçük bir metod olur.
+     * Bir Java programı ilgili işlemlerin çoğunluğunun başka metodlarda yapılır.
+     * Daha sonra ana metod diğer metodları basit çağırır. Böylece ana metodun içi kalabalık bir hale gelmemeiş olur.
+
+     *
+     * @param args bu uygulamada kullanılmamaktadır. Command Line üzerinden argüman alan
+     *             bir uygulamamız olsaydı kullanılacaktı.
+     */
+    public static void main(String[] args)
+    {
+        workWithData();
+    }
+}
