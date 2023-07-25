9f0e7f3d7320391417d2c6085c9a66d8cde23df1
Dogancan Kınık <dogancankinik@gmail.com>
2023-07-25 21:34:52 +0300

    Update Main.java

--- src/employeeApp/Main.java
+++ src/employeeApp/Main.java
@@ -16,9 +16,9 @@ public class Main
     }
 
     /**
-     * Bu projenin ana(main) metodu. Java uygulamalarında main metod küçük bir metod olur.
-     * Bir Java programı ilgili işlemlerin çoğunluğunun başka metodlarda yapılır.
-     * Daha sonra ana metod diğer metodları basit çağırır. Böylece ana metodun içi kalabalık bir hale gelmemeiş olur.
+     * Bu projenin ana(main) metodu. Java uygulamalarında main metot küçük bir metot olur.
+     * Bir Java programınd ilgili işlemlerin çoğunluğu başka metodlarda yapılır.
+     * Daha sonra ana metod diğer metodları çağırır. Böylece ana metodun içi kalabalık bir hale gelmememiş olur.
 
      *
      * @param args bu uygulamada kullanılmamaktadır. Command Line üzerinden argüman alan
